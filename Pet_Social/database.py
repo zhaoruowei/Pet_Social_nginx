d92493b99da859b5fd86c793b7be97a1d17c8403
Ruowei <zhaoruowei123@126.com>
2022-08-08 00:00:48 +0100

    update

--- Pet_Social/database.py
+++ Pet_Social/database.py
@@ -24,19 +24,29 @@ engines = {
 
 
 def config():
-    service_name = os.getenv("DATABASE_SERVICE_NAME", "").upper().replace("-", "_")
-    if service_name:
-        engine = engines.get(os.getenv("DATABASE_ENGINE"), engines["sqlite"])
+    if settings.DEBUG:
+        return {
+            'ENGINE': engines['mysql'],
+            'NAME': "petsocial",
+            'USER': "root",
+            'PASSWORD': "root123",
+            'HOST': "127.0.0.1",
+            'PORT': 3306,
+        }
     else:
-        engine = engines["sqlite"]
-    name = os.getenv("DATABASE_NAME")
-    if not name and engine == engines["sqlite"]:
-        name = os.path.join(settings.BASE_DIR, "db,sqlite3")
-    return {
-        'ENGINE': engine,
-        'NAME': name,
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('{}_SERVICE_HOST'.format(service_name)),
-        'PORT': os.getenv('{}_SERVICE_PORT'.format(service_name)),
-    }
+        service_name = os.getenv("DATABASE_SERVICE_NAME", "").upper().replace("-", "_")
+        if service_name:
+            engine = engines.get(os.getenv("DATABASE_ENGINE"), engines["sqlite"])
+        else:
+            engine = engines["sqlite"]
+        name = os.getenv("DATABASE_NAME")
+        if not name and engine == engines["sqlite"]:
+            name = os.path.join(settings.BASE_DIR, "db,sqlite3")
+        return {
+            'ENGINE': engine,
+            'NAME': name,
+            'USER': os.getenv('DATABASE_USER'),
+            'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+            'HOST': os.getenv('{}_SERVICE_HOST'.format(service_name)),
+            'PORT': os.getenv('{}_SERVICE_PORT'.format(service_name)),
+        }
