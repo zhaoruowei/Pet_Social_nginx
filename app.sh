889776fcc7cb8d68b6ccc1ce53f9676ed787f6c0
zhaoruowei <38174230+zhaoruowei@users.noreply.github.com>
2022-08-08 17:47:56 +0100

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-oc exec uwsgi --ini uwsgi.ini
+
+exec uwsgi \
+    --http-socket :8080 \
+    --die-on-term \
+    --master \
+    --single-interpreter \
+    --enable-threads \
+    --threads=5 \
+    --thunder-lock \
+    --static-map /static=static \
+    --module Pet_Social.wsgi
+
